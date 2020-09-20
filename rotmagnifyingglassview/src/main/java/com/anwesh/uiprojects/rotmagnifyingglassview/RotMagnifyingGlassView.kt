1337e3adaa0704ca2fb522494fc801f9793d352e
Anwesh43 <anweshthecool0@gmail.com>
2020-09-21 03:14:48 +0530

    adding RMGNode class

--- rotmagnifyingglassview/src/main/java/com/anwesh/uiprojects/rotmagnifyingglassview/RotMagnifyingGlassView.kt
+++ rotmagnifyingglassview/src/main/java/com/anwesh/uiprojects/rotmagnifyingglassview/RotMagnifyingGlassView.kt
@@ -126,4 +126,45 @@ class RotMagnifyingGlassView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RMGNode(var i : Int, val state : State = State()) {
+
+        private var next : RMGNode? = null
+        private var prev : RMGNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RMGNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRMGNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RMGNode {
+            var curr : RMGNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
