c8e4b514c40900a9cf0e8c766a299d0e00e8e7f9
Anwesh43 <anweshthecool0@gmail.com>
2020-09-21 03:28:57 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedrotmagnifyingglassview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrotmagnifyingglassview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedrotmagnifyingglassview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.rotmagnifyingglassview.RotMagnifyingGlassView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RotMagnifyingGlassView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
