3ed14e5eaabd1412f3de01623c5c8905b1af3673
Anwesh43 <anweshthecool0@gmail.com>
2020-09-21 02:53:58 +0530

    adding constants and libraries

--- rotmagnifyingglassview/src/main/java/com/anwesh/uiprojects/rotmagnifyingglassview/RotMagnifyingGlassView.kt
+++ rotmagnifyingglassview/src/main/java/com/anwesh/uiprojects/rotmagnifyingglassview/RotMagnifyingGlassView.kt
@@ -3,3 +3,26 @@ package com.anwesh.uiprojects.rotmagnifyingglassview
 /**
  * Created by anweshmishra on 21/09/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#03A9F4",
+        "#009688",
+        "#FFC107",
+        "#9C27B0"
+).map({Color.parseColor(it)}).toTypedArray()
+val rFactor : Float = 9.8f
+val lineFactor : Float = 3.2f
+val strokeFactor : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val rot : Float = 30f
