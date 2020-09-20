3b1db5b7fd8171a323c1bfb3eae7aade8ec61a1c
Anwesh43 <anweshthecool0@gmail.com>
2020-09-21 03:22:01 +0530

    adding static create method

--- rotmagnifyingglassview/src/main/java/com/anwesh/uiprojects/rotmagnifyingglassview/RotMagnifyingGlassView.kt
+++ rotmagnifyingglassview/src/main/java/com/anwesh/uiprojects/rotmagnifyingglassview/RotMagnifyingGlassView.kt
@@ -215,4 +215,13 @@ class RotMagnifyingGlassView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : RotMagnifyingGlassView {
+            val view : RotMagnifyingGlassView = RotMagnifyingGlassView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
