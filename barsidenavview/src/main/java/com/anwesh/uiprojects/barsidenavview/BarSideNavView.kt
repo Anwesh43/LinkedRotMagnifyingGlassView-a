a8184a3a9151bf0ea3ae2542e89426041af7a612
Anwesh43 <anweshthecool0@gmail.com>
2020-09-22 02:52:36 +0530

    adding barsidenavview constatns

--- barsidenavview/src/main/java/com/anwesh/uiprojects/barsidenavview/BarSideNavView.kt
+++ barsidenavview/src/main/java/com/anwesh/uiprojects/barsidenavview/BarSideNavView.kt
@@ -3,3 +3,26 @@ package com.anwesh.uiprojects.barsidenavview
 /**
  * Created by anweshmishra on 22/09/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#009688",
+        "#3F51B5",
+        "#3F51B5",
+        "#4CAF50"
+).map({Color.parseColor(it)}).toTypedArray()
+val lines : Int = 3
+val parts : Int = lines + 1
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 1.8f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
