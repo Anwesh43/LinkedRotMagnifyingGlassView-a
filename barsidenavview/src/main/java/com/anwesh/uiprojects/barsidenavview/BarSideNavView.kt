3fef83e02d5deb3d26c6b5de215a8bb1c2cb3cfa
Anwesh43 <anweshthecool0@gmail.com>
2020-09-22 03:06:13 +0530

    adding BSNNode class

--- barsidenavview/src/main/java/com/anwesh/uiprojects/barsidenavview/BarSideNavView.kt
+++ barsidenavview/src/main/java/com/anwesh/uiprojects/barsidenavview/BarSideNavView.kt
@@ -117,4 +117,45 @@ class BarSideNavView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BSNNode(var i : Int, val state : State = State()) {
+
+        private var next : BSNNode? = null
+        private var prev : BSNNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BSNNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBSNNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit): BSNNode {
+            var curr : BSNNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
