8b50e52e406a7db3c5aaf0e6a900c29404b9e25e
Anwesh43 <anweshthecool0@gmail.com>
2020-09-22 03:14:02 +0530

    adding BarSideNav class

--- barsidenavview/src/main/java/com/anwesh/uiprojects/barsidenavview/BarSideNavView.kt
+++ barsidenavview/src/main/java/com/anwesh/uiprojects/barsidenavview/BarSideNavView.kt
@@ -158,4 +158,27 @@ class BarSideNavView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BarSideNav(var i : Int) {
+
+        private var curr : BSNNode = BSNNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
