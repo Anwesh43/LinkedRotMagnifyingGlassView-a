7401ced38091da972522ffb00862f5e85ceaf526
Anwesh43 <anweshthecool0@gmail.com>
2020-09-22 03:19:48 +0530

    adding BarSideNavView in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedrotmagnifyingglassview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrotmagnifyingglassview/MainActivity.kt
@@ -3,13 +3,16 @@ package com.anwesh.uiprojects.linkedrotmagnifyingglassview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.anwesh.uiprojects.rotmagnifyingglassview.RotMagnifyingGlassView
+import com.anwesh.uiprojects.barsidenavview.BarSideNavView
+
+//import com.anwesh.uiprojects.rotmagnifyingglassview.RotMagnifyingGlassView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        RotMagnifyingGlassView.create(this)
+        //RotMagnifyingGlassView.create(this)
+        BarSideNavView.create(this)
         fullScreen()
     }
 }
